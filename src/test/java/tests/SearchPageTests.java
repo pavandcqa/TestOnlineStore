aa29eb615ea923f9e4c3c06904930e5665f6ce22
pavan.k <kaparthi.pavan@gmail.com>
2021-03-16 11:17:26 +0530

    created new tests for search

---
+++ src/test/java/tests/SearchPageTests.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+import base.TestBase;
+
+public class SearchPageTests extends TestBase {
+
+	@Test
+	public void verifySearchFeature()
+	{
+		System.out.println("This is search feature..");
+	}
+}
