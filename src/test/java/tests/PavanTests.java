bb5373e5b0100915b371beee3e369393728d134a
Pavan kaparthi <dcqadevops@gmail.com>
2021-11-24 15:28:18 +0530

    new tests cases added

---
+++ src/test/java/tests/PavanTests.java
@@ -0,0 +1,15 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+import base.TestBase;
+
+public class PavanTests extends TestBase{
+
+	@Test
+	public void testCase() {
+		
+		
+		// New test is been added
+	}
+}
